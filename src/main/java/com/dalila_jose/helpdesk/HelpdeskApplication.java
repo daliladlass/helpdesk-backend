a2f91dd1d1d94dc63df6fccbbcd78cefa9b8ad05
daliladlass <dlassdalila@gmail.com>
2023-04-19 01:37:59 -0300

    projeto iniciado

---
+++ src/main/java/com/dalila_jose/helpdesk/HelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.dalila_jose.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+}
